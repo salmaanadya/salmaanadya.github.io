2d09a2dad5bf6a506e03009863157534885fff89
salmaanadya <salmanadyashafar@gmail.com>
2023-04-15 21:24:53 +0700

    first commit

---
+++ choose.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Chat System</title>
+    <body bgcolor="pink">
+        <font size="5pt" face="Calibri" color="black">
+
+        </head><center>
+            Sistem pendukung keputusan berbasis komunikasi dan kelompok menggunakan berbagai alat komunikasi -- seperti email, pesan instan, atau voice chat -- untuk memungkinkan lebih dari satu orang mengerjakan tugas yang sama. Tujuan di balik jenis DSS ini adalah untuk meningkatkan kolaborasi antara pengguna dan sistem dan untuk meningkatkan efisiensi dan efektivitas sistem secara keseluruhan
+            <br />
+            <p><h1>Choose Below to Continue</h1><a><strong>
+                <a href="@@cht">Group Chat</p></a>
+                <br /><br>
+            </br></br><a href="logout.php">Log Out</a>
+            <body>
+
+            </body>
+            <br /><br><br /><br><br /><br><br />
+            <p><Strong><center> UNY</center></Strong></p>
+            </html>
